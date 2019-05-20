de3d7442e1f9df73c5aacd7153ad10b2830cc351
uri parush <48644756+uri-p@users.noreply.github.com>
2019-05-20 18:26:08 +0300

    Update bd_to_prod.sh

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -30,8 +30,8 @@ sudo pip install --upgrade bumpversion
 bumpversion patch --message "{current_version} → {new_version}. Changes: ${changes}"
 
     echo "Uploading lumigo java tracer to maven central repository"
-sudo cp -rf maven/settings.xml /usr/share/maven/conf
-mvn clean deploy
+#sudo cp -rf maven/settings.xml /usr/share/maven/conf
+#mvn clean deploy
 
 echo "Create release tag"
 push_tags
