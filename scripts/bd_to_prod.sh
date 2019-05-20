e25a965f371eb6f0ed62f44b3f152e371a2a796c
uri parush <48644756+uri-p@users.noreply.github.com>
2019-05-20 19:28:31 +0300

    Update bd_to_prod.sh

--- scripts/bd_to_prod.sh
+++ scripts/bd_to_prod.sh
@@ -29,9 +29,10 @@ changes=$(git log $(git describe --tags --abbrev=0)..HEAD --oneline)
 sudo pip install --upgrade bumpversion
 bumpversion patch --message "{current_version} → {new_version}. Changes: ${changes}"
 
-    echo "Uploading lumigo java tracer to maven central repository"
-#sudo cp -rf maven/settings.xml /usr/share/maven/conf
-#mvn clean deploy
+echo "Uploading lumigo java tracer to maven central repository"
+sudo cp -rf maven/settings.xml /usr/share/maven/conf
+echo -e "$GPG_KEY" | gpg --import
+mvn clean deploy
 
 echo "Create release tag"
 push_tags
