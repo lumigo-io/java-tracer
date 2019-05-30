c321b68ec17da5cdcd0e39323fe21af58058de0b
uriparush <uri@lumigo.io>
2019-05-30 18:10:58 +0300

    add log messages

--- src/main/java/io/lumigo/core/instrumentation/agent/Loader.java
+++ src/main/java/io/lumigo/core/instrumentation/agent/Loader.java
@@ -2,9 +2,11 @@ package io.lumigo.core.instrumentation.agent;
 
 import io.lumigo.core.instrumentation.impl.ApacheHttpInstrumentation;
 import net.bytebuddy.agent.builder.AgentBuilder;
+import org.pmw.tinylog.Logger;
 
 public class Loader {
     public static void instrument(java.lang.instrument.Instrumentation inst) {
+        Logger.info("Start Instrumentation");
         ApacheHttpInstrumentation instrumentation = new ApacheHttpInstrumentation();
         AgentBuilder builder =
                 new AgentBuilder.Default()
@@ -13,5 +15,6 @@ public class Loader {
                         .type(instrumentation.getTypeMatcher())
                         .transform(instrumentation.getTransformer());
         builder.installOn(inst);
+        Logger.info("Finish Instrumentation");
     }
 }
