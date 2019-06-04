ba4664b073632bd0d6365645236808d740cc3da1
uriparush <uri@lumigo.io>
2019-06-04 12:05:45 +0300

    create new agent loading

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-mvn -f agent/pom.xml clean package
+mvn -f agent/pom.xml clean install
 mvn -Dmaven.test.skip=true clean install
--- src/main/java/io/lumigo/core/instrumentation/agent/Installer.java
+++ src/main/java/io/lumigo/core/instrumentation/agent/Installer.java
@@ -3,7 +3,11 @@ package io.lumigo.core.instrumentation.agent;
 import com.sun.tools.attach.VirtualMachine;
 import com.sun.tools.attach.VirtualMachineDescriptor;
 import io.lumigo.core.configuration.Configuration;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 import org.pmw.tinylog.Logger;
 
 public class Installer {
@@ -16,12 +20,17 @@ public class Installer {
             firstStart = false;
             Logger.info("Agent installation start");
             List<VirtualMachineDescriptor> vms = VirtualMachine.list();
-
+            String agentPath = findAgentPath();
+            Logger.info("Loading agent jar: {}", agentPath);
             for (VirtualMachineDescriptor vmd : vms) {
                 try {
                     VirtualMachine vm = VirtualMachine.attach(vmd.id());
                     try {
-                        vm.loadAgent("/var/task/lumigo-agent.jar");
+                        if (agentPath.contains("lib")) {
+                            vm.loadAgent(agentPath, "lib");
+                        } else {
+                            vm.loadAgent(agentPath);
+                        }
                     } finally {
                         vm.detach();
                     }
@@ -35,6 +44,25 @@ public class Installer {
                     Configuration.getInstance().isAwsEnvironment(),
                     Configuration.getInstance().isInstrumentationEnabled(),
                     firstStart);
+        }
+    }
+
+    public static String findAgentPath() {
+        if (Files.notExists(Paths.get("/var/task/lumigo-agent.jar"))) {
+            Logger.info(
+                    "Agent jar was not found under /var/task/lumigo-agent.jar, try to find it under /var/task/lib");
+            try (Stream<Path> paths = Files.walk(Paths.get("/var/task/lib"))) {
+                Path lumigoAgentPath =
+                        paths.filter(p -> p.toFile().getAbsolutePath().contains("lumigo-agent"))
+                                .findFirst()
+                                .get();
+                return lumigoAgentPath.toFile().getAbsolutePath();
+            } catch (Exception e) {
+                Logger.error(e);
+                return "/var/task/lumigo-agent.jar";
+            }
+        } else {
+            return "/var/task/lumigo-agent.jar";
         }
     }
 }
