1c125fb1ef60346fa8041bf482db89661a0d95af
CircleCI <no-reply@build.com>
2019-06-10 16:55:03 +0000

    1.0.16 → 1.0.17. Changes: f07be8a Merge pull request #17 from lumigo-io/RD-742-Alternative-way-to-run-the-the-JVM-tracer c4a0189 change name of method return value void test 3357aa6 change name of method return value void test c3d58dc add new executor implementation

--- src/main/java/io/lumigo/core/configuration/Configuration.java
+++ src/main/java/io/lumigo/core/configuration/Configuration.java
@@ -75,7 +75,7 @@ public class Configuration {
     }
 
     public String getLumigoTracerVersion() {
-        return "1.0.16";
+        return "1.0.17";
     }
 
     public Duration getLumigoTimeout() {
