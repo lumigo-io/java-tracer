f1d5ccd8a6835611907e76d9b78070d13190c20e
uriparush <uri@lumigo.io>
2019-06-26 16:06:02 +0300

    RD-784 ADD SNS SUPPORT TO JAVA TRACER

--- src/test/java/io/lumigo/core/parsers/AwsParserFactoryTest.java
+++ src/test/java/io/lumigo/core/parsers/AwsParserFactoryTest.java
@@ -1,21 +1,23 @@
 package io.lumigo.core.parsers;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class AwsParserFactoryTest {
 
     @Test
-    public void test_check_non_supported_value(){
-        assertEquals(DefaultParser.class,AwsParserFactory.getParser("Not supported").getClass());
+    public void test_check_non_supported_value() {
+        assertEquals(DefaultParser.class, AwsParserFactory.getParser("Not supported").getClass());
     }
+
     @Test
-    public void test_check_null_value(){
-        assertEquals(DefaultParser.class,AwsParserFactory.getParser("Not supported").getClass());
+    public void test_check_null_value() {
+        assertEquals(DefaultParser.class, AwsParserFactory.getParser("Not supported").getClass());
     }
+
     @Test
-    public void test_check_sns_value(){
-        assertEquals(SnsParser.class,AwsParserFactory.getParser("AmazonSNS").getClass());
+    public void test_check_sns_value() {
+        assertEquals(SnsParser.class, AwsParserFactory.getParser("AmazonSNS").getClass());
     }
 }
