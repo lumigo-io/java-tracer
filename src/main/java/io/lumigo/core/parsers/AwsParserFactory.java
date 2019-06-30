fe338a7d1d139710ba18f98732adac3293f13b79
uriparush <uri@lumigo.io>
2019-06-30 10:16:54 +0300

    RD-784 ADD SNS SUPPORT TO JAVA TRACER

--- src/main/java/io/lumigo/core/parsers/AwsParserFactory.java
+++ src/main/java/io/lumigo/core/parsers/AwsParserFactory.java
@@ -1,6 +1,11 @@
 package io.lumigo.core.parsers;
 
 public class AwsParserFactory {
+    /**
+     *
+     * @param serviceName - AWS service name
+     * @return Relavant parser if exists or Default parser
+     */
     public static IAwsParser getParser(String serviceName) {
         if (serviceName == null) {
             return new DefaultParser();
