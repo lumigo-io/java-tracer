e1ef0d0acaca128ae5b85c39c8ead2a8e1ef9083
CircleCI <no-reply@build.com>
2020-06-17 12:02:36 +0000

    1.0.31 → 1.0.32. Changes: 2d9b62e Fix lambda direct invoke issue (#34)

--- src/main/java/io/lumigo/core/configuration/Configuration.java
+++ src/main/java/io/lumigo/core/configuration/Configuration.java
@@ -81,7 +81,7 @@ public class Configuration {
     }
 
     public String getLumigoTracerVersion() {
-        return "1.0.31";
+        return "1.0.32";
     }
 
     public Duration getLumigoTimeout() {
