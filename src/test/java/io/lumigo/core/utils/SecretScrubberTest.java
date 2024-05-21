856669c3e02ef8676ba95b2e2ecb006618cf111d
Harel Moshe <harelm@lumigo.io>
2024-05-21 10:46:44 +0300

    Test scrubbing with AWS SDK

--- src/test/java/io/lumigo/core/utils/SecretScrubberTest.java
+++ src/test/java/io/lumigo/core/utils/SecretScrubberTest.java
@@ -14,27 +14,34 @@ class SecretScrubberTest {
     @Test
     @DisplayName("does not modify non-json payloads")
     void testSecretScrubbingUtils_does_not_scrub_non_json_payloads() {
-        assertEquals("123", new SecretScrubber(noScrubbingOverridesEnv).scrubStringifiedObject("123"));
+        assertEquals(
+                "123", new SecretScrubber(noScrubbingOverridesEnv).scrubStringifiedObject("123"));
     }
 
     @Test
     @DisplayName("scrubs a nested body value with default expressions")
     void testSecretScrubbingUtils_scrubs_json_payload_default() {
-        String actual = new SecretScrubber(noScrubbingOverridesEnv)
-                .scrubStringifiedObject(
-                        "{\"pass\":\"word\",\"key\":\"value\",\"secret\":\"stuff\",\"credential\":\"admin:admin\",\"passphrase\":\"SesameOpen\",\"SessionToken\":\"XyZ012x=\",\"x-amz-security-token\":\"amzToken123\",\"Signature\":\"yours truly\",\"authorization\":\"Bearer 123\"}");
-        String expected = "{\"authorization\":\"****\",\"credential\":\"****\",\"pass\":\"****\",\"SessionToken\":\"****\",\"Signature\":\"****\",\"passphrase\":\"****\",\"secret\":\"****\",\"x-amz-security-token\":\"****\",\"key\":\"****\"}";
+        String actual =
+                new SecretScrubber(noScrubbingOverridesEnv)
+                        .scrubStringifiedObject(
+                                "{\"pass\":\"word\",\"key\":\"value\",\"secret\":\"stuff\",\"credential\":\"admin:admin\",\"passphrase\":\"SesameOpen\",\"SessionToken\":\"XyZ012x=\",\"x-amz-security-token\":\"amzToken123\",\"Signature\":\"yours truly\",\"authorization\":\"Bearer 123\"}");
+        String expected =
+                "{\"authorization\":\"****\",\"credential\":\"****\",\"pass\":\"****\",\"SessionToken\":\"****\",\"Signature\":\"****\",\"passphrase\":\"****\",\"secret\":\"****\",\"x-amz-security-token\":\"****\",\"key\":\"****\"}";
 
         assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("scrubs body using the default patterns when LUMIGO_SECRET_MASKING_REGEX is invalid")
-    void testSecretScrubbingUtils_scrubs_json_payload_default_used_when_invalid_pattern_list_provided() {
-        String actual = new SecretScrubber(envWithMaskingRegex("[THIS IS NOT A VALID JSON ARRAY]"))
-                .scrubStringifiedObject(
-                        "{\"pass\":\"word\",\"key\":\"value\",\"secret\":\"stuff\",\"credential\":\"admin:admin\",\"passphrase\":\"SesameOpen\",\"SessionToken\":\"XyZ012x=\",\"x-amz-security-token\":\"amzToken123\",\"Signature\":\"yours truly\",\"authorization\":\"Bearer 123\"}");
-        String expected = "{\"authorization\":\"****\",\"credential\":\"****\",\"pass\":\"****\",\"SessionToken\":\"****\",\"Signature\":\"****\",\"passphrase\":\"****\",\"secret\":\"****\",\"x-amz-security-token\":\"****\",\"key\":\"****\"}";
+    @DisplayName(
+            "scrubs body using the default patterns when LUMIGO_SECRET_MASKING_REGEX is invalid")
+    void
+            testSecretScrubbingUtils_scrubs_json_payload_default_used_when_invalid_pattern_list_provided() {
+        String actual =
+                new SecretScrubber(envWithMaskingRegex("[THIS IS NOT A VALID JSON ARRAY]"))
+                        .scrubStringifiedObject(
+                                "{\"pass\":\"word\",\"key\":\"value\",\"secret\":\"stuff\",\"credential\":\"admin:admin\",\"passphrase\":\"SesameOpen\",\"SessionToken\":\"XyZ012x=\",\"x-amz-security-token\":\"amzToken123\",\"Signature\":\"yours truly\",\"authorization\":\"Bearer 123\"}");
+        String expected =
+                "{\"authorization\":\"****\",\"credential\":\"****\",\"pass\":\"****\",\"SessionToken\":\"****\",\"Signature\":\"****\",\"passphrase\":\"****\",\"secret\":\"****\",\"x-amz-security-token\":\"****\",\"key\":\"****\"}";
 
         assertEquals(expected, actual);
     }
@@ -42,10 +49,12 @@ class SecretScrubberTest {
     @Test
     @DisplayName("scrubs body using default expressions - case insensitive")
     void testSecretScrubbingUtils_scrubs_json_payload_default_case_insensitive() {
-        String actual = new SecretScrubber(noScrubbingOverridesEnv)
-                .scrubStringifiedObject(
-                        "{\"pAss\":\"word\",\"KeY\":\"value\",\"seCRet\":\"stuff\",\"CrEdEntial\":\"admin:admin\",\"paSSPhrase\":\"SesameOpen\",\"seSSIOntOkEn\":\"XyZ012x=\",\"X-AMZ-security-token\":\"amzToken123\",\"SIGnatUre\":\"yours truly\",\"AuTHOrization\":\"Bearer 123\"}");
-        String expected = "{\"pAss\":\"****\",\"CrEdEntial\":\"****\",\"paSSPhrase\":\"****\",\"seSSIOntOkEn\":\"****\",\"SIGnatUre\":\"****\",\"seCRet\":\"****\",\"X-AMZ-security-token\":\"****\",\"KeY\":\"****\",\"AuTHOrization\":\"****\"}";
+        String actual =
+                new SecretScrubber(noScrubbingOverridesEnv)
+                        .scrubStringifiedObject(
+                                "{\"pAss\":\"word\",\"KeY\":\"value\",\"seCRet\":\"stuff\",\"CrEdEntial\":\"admin:admin\",\"paSSPhrase\":\"SesameOpen\",\"seSSIOntOkEn\":\"XyZ012x=\",\"X-AMZ-security-token\":\"amzToken123\",\"SIGnatUre\":\"yours truly\",\"AuTHOrization\":\"Bearer 123\"}");
+        String expected =
+                "{\"pAss\":\"****\",\"CrEdEntial\":\"****\",\"paSSPhrase\":\"****\",\"seSSIOntOkEn\":\"****\",\"SIGnatUre\":\"****\",\"seCRet\":\"****\",\"X-AMZ-security-token\":\"****\",\"KeY\":\"****\",\"AuTHOrization\":\"****\"}";
 
         assertEquals(expected, actual);
     }
@@ -53,8 +62,9 @@ class SecretScrubberTest {
     @Test
     @DisplayName("scrubs body using LUMIGO_SECRET_MASKING_REGEX - top-level key")
     void testSecretScrubbingUtils_scrubs_json_payload_top_level_key() {
-        String actual = new SecretScrubber(envWithMaskingRegex("[\".*topsecret.*\"]"))
-                .scrubStringifiedObject("{\"topsecret\":\"stuff\"}");
+        String actual =
+                new SecretScrubber(envWithMaskingRegex("[\".*topsecret.*\"]"))
+                        .scrubStringifiedObject("{\"topsecret\":\"stuff\"}");
         String expected = "{\"topsecret\":\"****\"}";
 
         assertEquals(expected, actual);
@@ -63,8 +73,9 @@ class SecretScrubberTest {
     @Test
     @DisplayName("scrubs body using LUMIGO_SECRET_MASKING_REGEX - nested keys")
     void testSecretScrubbingUtils_scrubs_json_payload_nested_key() {
-        String actual = new SecretScrubber(envWithMaskingRegex("[\".*topsecret.*\"]"))
-                .scrubStringifiedObject("{\"some\": {\"topsecret\":\"stuff\"}, \"a\": 1}");
+        String actual =
+                new SecretScrubber(envWithMaskingRegex("[\".*topsecret.*\"]"))
+                        .scrubStringifiedObject("{\"some\": {\"topsecret\":\"stuff\"}, \"a\": 1}");
 
         String expected = "{\"some\":{\"topsecret\":\"****\"},\"a\":1}";
 
