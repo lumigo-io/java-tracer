002be9d5e2b3cb33ac4e5faa0cea05b8c063cef7
nadav <nadavg@lumigo.io>
2024-06-24 17:19:12 +0300

    README

--- layer/generate.sh
+++ layer/generate.sh
@@ -4,5 +4,3 @@ cp ../target/java-tracer-1.0.47.jar lumigo/lumigo-tracer.jar
 cp ../agent/target/lumigo-agent-1.0.47.jar lumigo/lumigo-agent.jar
 
 zip -r lumigo-java-layer.zip lumigo
-
-rm -rf lumigo
